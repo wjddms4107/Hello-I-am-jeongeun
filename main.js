55eb7ae86af33411d8d5483179eb343c1cbec49c
wjddms4107 <wjdms4107@naver.com>
2022-06-15 16:27:43 +0900

    js todolist 추가

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let swiper = new Swiper(".mySwiper", {
 
 
 const fadeEls = document.querySelectorAll('.fade-in');
-fadeEls.forEach(function(fadeEl, index) {
+fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
     delay: (index + 1) * 1.2,
     opacity: 1,
@@ -28,3 +28,48 @@ fadeEls.forEach(function(fadeEl, index) {
   });
 });
 
+
+const ul = document.querySelector('.lists');
+const inputList = document.querySelector('.input_list');
+const listAdd = document.querySelector('.list_add');
+
+
+const addList = () => {
+  const text = inputList.value;
+  if (inputList.value === '') {
+    inputList.focus();
+    return;
+  }
+
+  const li = document.createElement('li');
+  li.setAttribute('class', 'list');
+
+  const listLabel = document.createElement('label');
+  listLabel.setAttribute('class','list_label');
+
+  listLabel.innerHTML = text;
+  
+  const listInput = document.createElement('input');
+  listInput.setAttribute('type', 'checkbox');
+  listInput.setAttribute('name', 'todolist');
+  listInput.setAttribute('class', 'input_todolist');
+
+  
+
+  li.appendChild(listLabel);
+  li.appendChild(listInput);
+  ul.appendChild(li);
+  inputList.value = '';
+  inputList.focus();
+}
+
+listAdd.addEventListener('click', () => {
+  addList();
+});
+
+inputList.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    addList();
+  }
+  return;
+});
